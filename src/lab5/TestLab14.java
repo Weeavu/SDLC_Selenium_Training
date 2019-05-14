10ff97b73dda27080e8044f5c009a03cf532e7eb
choitali chakraborty <choitali@buffalo.edu>
2019-05-13 23:23:33 -0400

    adding reading excel data

--- src/lab5/TestLab14.java
+++ src/lab5/TestLab14.java
@@ -3,71 +3,104 @@ package lab5;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class TestLab14 {
 
 	    public static void main(String[] args) throws Exception {
-	            
-	    	//reading from an excel file
+	    	
 
-	    	try {
+	        try {
+	            File excel = new File("resources//test.xlsx");
+	            FileInputStream fis = new FileInputStream(excel);
+	            XSSFWorkbook book = new XSSFWorkbook(fis);
+	            XSSFSheet sheet = book.getSheetAt(0);
 
-	    	FileInputStream excelFile = new FileInputStream(new File("test.xlsx"));
+	            Iterator<Row> itr = sheet.iterator();
 
-	    	Workbook workbook = new XSSFWorkbook(excelFile);
+	            // Iterating over Excel file in Java
+	            while (itr.hasNext()) {
+	                Row row = itr.next();
 
-	    	Sheet datatypeSheet = workbook.getSheetAt(0);
+	                // Iterating over each column of Excel file
+	                Iterator<Cell> cellIterator = row.cellIterator();
+	                while (cellIterator.hasNext()) {
 
-	    	Iterator<Row> iterator = datatypeSheet.iterator();
+	                    Cell cell = cellIterator.next();
 
-	    	while (iterator.hasNext()) {
+	                    switch (cell.getCellType()) {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue() + "\t");
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue() + "\t");
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue() + "\t");
+	                        break;
+	                    default:
 
-	    	Row currentRow = iterator.next();
+	                    }
+	                }
+	                System.out.println("");
+	            }
 
-	    	Iterator<Cell> cellIterator = currentRow.iterator();
+	            // writing data into XLSX file
+	            Map<String, Object[]> newData = new HashMap<String, Object[]>();
+	            newData.put("1", new Object[] { 1d, "Raju", "75K", "dev",
+	                    "SGD" });
+	            newData.put("2", new Object[] { 2d, "Ramesh", "58K", "test",
+	                    "USD" });
+	            newData.put("3", new Object[] { 3d, "Ravi", "90K", "PMO",
+	                    "INR" });
 
-	    	while (cellIterator.hasNext()) {
+	            Set<String> newRows = newData.keySet();
+	            int rownum = sheet.getLastRowNum();
 
-	    	Cell currentCell = cellIterator.next();
+	            for (String key : newRows) {
+	                Row row = sheet.createRow(rownum++);
+	                Object[] objArr = newData.get(key);
+	                int cellnum = 0;
+	                for (Object obj : objArr) {
+	                    Cell cell = row.createCell(cellnum++);
+	                    if (obj instanceof String) {
+	                        cell.setCellValue((String) obj);
+	                    } else if (obj instanceof Boolean) {
+	                        cell.setCellValue((Boolean) obj);
+	                    } else if (obj instanceof Date) {
+	                        cell.setCellValue((Date) obj);
+	                    } else if (obj instanceof Double) {
+	                        cell.setCellValue((Double) obj);
+	                    }
+	                }
+	            }
 
-	    	//getCellTypeEnum shown as deprecated for version 3.15
+	            // open an OutputStream to save written data into Excel file
+	            FileOutputStream os = new FileOutputStream(excel);
+	            book.write(os);
+	            System.out.println("Writing on Excel file Finished ...");
 
-	    	//getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+	            // Close workbook, OutputStream and Excel file to prevent leak
+	            os.close();
+	            book.close();
+	            fis.close();
 
-	    	if (currentCell.getCellTypeEnum() == CellType.STRING) {
-
-	    	System.out.print(currentCell.getStringCellValue() + "--");
-
-	    	} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-
-	    	System.out.print(currentCell.getNumericCellValue() + "--");
-
-	    	}
-
-	    	}
-
-	    	System.out.println();
-
-	    	}
-
-	    	} catch (FileNotFoundException e) {
-
-	    	e.printStackTrace();
-
-	    	} catch (IOException e) {
-
-	    	e.printStackTrace();
-
-	    	}
+	        } catch (FileNotFoundException fe) {
+	            fe.printStackTrace();
+	        } catch (IOException ie) {
+	            ie.printStackTrace();
+	        }
 	    }
 
 
